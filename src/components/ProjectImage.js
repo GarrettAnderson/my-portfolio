d6979772e77bb199683288e852318ee55fc25f91
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-07-15 11:54:37 -0400

    working on the project section component and still having trouble with the click through to the individual project details page. going to update the homepage component to remove the ProjectSection component and add the full code instead to see if i can get the data via props

--- src/components/ProjectImage.js
+++ src/components/ProjectImage.js
@@ -1,17 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import data from '../data/projects.json'
-import colorPickerImg from '../images/colorPickerImg.png'
-import bombSnifferImg from '../images/bombSnifferImg.png'
-import toTheMoviesImg from '../images/toTheMoviesImg.png'
-import capstoneYogiMe from '../images/capstoneYogiMe.png'
-
-const projects = [
-  { id: 1, source: colorPickerImg, name: 'Color Picker' },
-  { id: 2, source: bombSnifferImg, name: 'Minesweeper' },
-  { id: 3, source: toTheMoviesImg, name: 'To The Movies' },
-  { id: 4, source: capstoneYogiMe, name: 'Yogi Me' }
-]
 
 class ProjectImage extends Component {
   state = {
@@ -19,7 +6,7 @@ class ProjectImage extends Component {
   }
 
   componentDidMount() {
-    console.log(projects[this.props.match.params.id])
+    console.log('page load')
     console.log(this.props)
   }
 
--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -1,29 +1,33 @@
 import React, { Component } from 'react'
-import ProjectImage from '../components/ProjectImage'
-// import data from '../data/projects.json'
+import { Link } from 'react-router-dom'
 import colorPickerImg from '../images/colorPickerImg.png'
 import bombSnifferImg from '../images/bombSnifferImg.png'
 import toTheMoviesImg from '../images/toTheMoviesImg.png'
 import capstoneYogiMe from '../images/capstoneYogiMe.png'
 
-const projects = [
-  { id: 1, source: colorPickerImg, name: 'Color Picker' },
-  { id: 2, source: bombSnifferImg, name: 'Minesweeper' },
-  { id: 3, source: toTheMoviesImg, name: 'To The Movies' },
-  { id: 4, source: capstoneYogiMe, name: 'Yogi Me' }
-]
-
 class ProjectSection extends Component {
   state = {
-    projects: []
+    projects: [
+      { id: 1, source: colorPickerImg, name: 'Color Picker' },
+      { id: 2, source: bombSnifferImg, name: 'Minesweeper' },
+      { id: 3, source: toTheMoviesImg, name: 'To The Movies' },
+      { id: 4, source: capstoneYogiMe, name: 'Yogi Me' }
+    ]
   }
 
   render() {
     return (
       <ol>
-        {projects.map((image) => {
+        {this.state.projects.map((image) => {
           console.log(image)
-          return <ProjectImage key={image.id} id={image.id} url={image.source} name={image.name} />
+          return (
+            <li key={image.id}>
+              <Link to={`/project/${image.id}`}>
+                <img alt="project" src={image.source} id={image.id} />
+                <h3>{image.name}</h3>
+              </Link>
+            </li>
+          )
         })}
       </ol>
     )
