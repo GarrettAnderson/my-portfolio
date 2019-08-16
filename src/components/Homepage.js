9568e51643b7c264d8404a7eb7fca484cbc0ed9d
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-08-16 13:56:21 -0400

    updated the json data for the project descriptions and tech stack

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -81,11 +81,11 @@ class Homepage extends Component {
               method="POST"
             >
               <fieldset id="fs-form-inputs">
-                <label htmlFor="full-name">Full Name</label>
+                {/* <label htmlFor="full-name">Full Name</label> */}
                 <input type="text" name="name" id="full-name" placeholder="First and Last" required="" />
-                <label htmlFor="email-address">Email Address</label>
+                {/* <label htmlFor="email-address">Email Address</label> */}
                 <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="" />
-                <label htmlFor="message">Message</label>
+                {/* <label htmlFor="message">Message</label> */}
                 <textarea
                   rows="5"
                   name="message"
