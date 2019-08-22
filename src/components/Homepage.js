5cd9d435379412ccc0fdd2f8eb5d9ebb97f1da71
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2019-08-22 15:24:38 -0400

    working on the design and updated the contact and projects section

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -38,7 +38,7 @@ class Homepage extends Component {
           <section className="profile-img-container">
             <section className="profileImg-with-text">
               <img src={profileImg} alt="headshot" className="profile-image" />
-              <h3>Garrett Lee Graham Anderson</h3>
+              <h3 className="headshot-name">Garrett Lee Graham Anderson</h3>
               <section className="social-media-icons">
                 <ol>
                   <li>
@@ -76,58 +76,67 @@ class Homepage extends Component {
               focusing on how to best represent my work and the work I accomplish for clients and employers alike.
             </p>
           </article>
-          <section className="projects" id="favorite-projects">
-            <h3>Projects</h3>
-            <ol>
-              {this.state.projects.map((image) => {
-                console.log(image)
-                return (
-                  <li key={image.id}>
-                    <Link to={`/${image.id}`}>
-                      <img alt="project" src={image.source} id={image.id} />
-                      <h4>{image.name}</h4>
-                    </Link>
-                  </li>
-                )
-              })}
-            </ol>
-          </section>
 
-          <section className="contact-form-section" id="contact-me">
-            <h3>Contact</h3>
-            <form
-              className="contact-form"
-              id="fs-frm"
-              acceptCharset="utf-8"
-              action="https://formspree.io/garrettleegrahamanderson@gmail.tld"
-              method="POST"
-            >
-              <fieldset id="fs-form-inputs">
-                <ol class="contact-list-container">
-                  <li>
-                    <input type="text" name="name" id="full-name" placeholder="First and Last" required="" />
-                  </li>
-                  <li>
-                    <input type="email" name="_replyto" id="email-address" placeholder="email@domain.tld" required="" />
-                  </li>
-                  <li>
-                    <textarea
-                      rows="5"
-                      name="message"
-                      id="message"
-                      placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus."
-                      required=""
-                    />
-                  </li>
-                  <li>
-                    <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
-                  </li>
-                  <li>
-                    <input type="submit" value="send" className="send-btn" />
-                  </li>
-                </ol>
-              </fieldset>
-            </form>
+          <section class="project-contact-sections-container">
+            <section className="projects" id="favorite-projects">
+              <h3>Projects</h3>
+              <ol>
+                {this.state.projects.map((image) => {
+                  console.log(image)
+                  return (
+                    <li key={image.id}>
+                      <Link to={`/${image.id}`}>
+                        <img alt="project" src={image.source} id={image.id} />
+                        {/* <h4>{image.name}</h4> */}
+                      </Link>
+                    </li>
+                  )
+                })}
+              </ol>
+            </section>
+
+            <section className="contact-form-section" id="contact-me">
+              <h3>Contact</h3>
+              <form
+                className="contact-form"
+                id="fs-frm"
+                acceptCharset="utf-8"
+                action="https://formspree.io/garrettleegrahamanderson@gmail.tld"
+                method="POST"
+              >
+                <fieldset id="fs-form-inputs">
+                  <ol class="contact-list-container">
+                    <li>
+                      <input type="text" name="name" id="full-name" placeholder="First and Last" required="" />
+                    </li>
+                    <li>
+                      <input
+                        type="email"
+                        name="_replyto"
+                        id="email-address"
+                        placeholder="email@domain.tld"
+                        required=""
+                      />
+                    </li>
+                    <li>
+                      <textarea
+                        rows="5"
+                        name="message"
+                        id="message"
+                        placeholder="Aenean lacinia bibendum nulla sed consectetur. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla nullam quis risus."
+                        required=""
+                      />
+                    </li>
+                    <li>
+                      <input type="hidden" name="_subject" id="email-subject" value="Contact Form Submission" />
+                    </li>
+                    <li>
+                      <input type="submit" value="send" className="send-btn" />
+                    </li>
+                  </ol>
+                </fieldset>
+              </form>
+            </section>
           </section>
         </main>
         <footer>
