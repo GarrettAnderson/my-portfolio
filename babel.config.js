bb63d8c465bc8f2eef3025c54ec87e13e1409f2b
Garrett <garrettleegrahamanderson@gmail.com>
2023-05-13 17:37:22 -0400

    removed the babel files. got the font awesome icons to render

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (api) {
-  api.cache.never();
-  return {
-    plugins: ["macros"],
-  };
-};
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,15 @@ import React from "react";
 import ResumeFile from "../../assets/files/GarrettAndersonResume2023.pdf";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBootstrap,
+  faHtml5,
+  faCss3Alt,
+  faJs,
+  faNodeJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Resume(props) {
   return (
@@ -20,19 +29,31 @@ export default function Resume(props) {
         </div>
         <div className="col-sm-12 col-md-6 skills">
           <h1>Skills</h1>
-          <ul class="list-group list-group-flush">
+          <ul className="list-group list-group-flush">
             {/* <FontAwesomeIcon icon={icon({ name: "user-secret" })} /> */}
             <li></li>
-            <li class="list-group-item">HTML5</li>
-            <li class="list-group-item">CSS3</li>
-            <li class="list-group-item">JavaScript</li>
-            <li class="list-group-item">jQuery</li>
-            <li class="list-group-item">ReactJS</li>
-            <li class="list-group-item">bootstrap</li>
-            <li class="list-group-item">Node.js</li>
-            <li class="list-group-item">JSON</li>
-            <li class="list-group-item">SQL</li>
-            <li class="list-group-item">Express.js</li>
+            <li className="list-group-item">
+              <FontAwesomeIcon icon={faHtml5} />
+            </li>
+            <li class="list-group-item">
+              <FontAwesomeIcon icon={faCss3Alt} />
+            </li>
+            <li class="list-group-item">
+              <FontAwesomeIcon icon={faJs} />
+            </li>
+            {/* <li class="list-group-item">jQuery</li> */}
+            <li class="list-group-item">
+              <FontAwesomeIcon icon={faReact} />
+            </li>
+            <li class="list-group-item">
+              <FontAwesomeIcon icon={faBootstrap} />
+            </li>
+            <li class="list-group-item">
+              <FontAwesomeIcon icon={faNodeJs} />
+            </li>
+            {/* <li class="list-group-item">JSON</li> */}
+            {/* <li class="list-group-item">SQL</li>
+            <li class="list-group-item">Express.js</li> */}
           </ul>
         </div>
       </div>
